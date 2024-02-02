364771cc4eacbccb6a1c95c5aa27c825aa8fde44
Sandip Bera <sandipbera.etc@gmail.com>
2024-02-02 18:38:28 +0530

    vfsdvdb

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import { useState } from 'react';
 
 function App() {
+  const [mystate,setmystaate] = useState(false);
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </>
+    // <>
+    //   <Routes>
+    //     <Route path='/' element={<Home />} />
+    //   </Routes>
+    // </>
+    <div>
+      Hello World
+    </div>
   );
 }
 
