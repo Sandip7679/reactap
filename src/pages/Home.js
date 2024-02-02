06fa5a8e0893a26e43d6d31809df9aadd64a5094
Sandip Bera <sandipbera.etc@gmail.com>
2024-02-02 17:34:12 +0530

    gvdfb

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Home = () => {
-    const [contactModalStatus, setcontactModalStatus] = useState({ show: false, data: {} });
+    // const [contactModalStatus, setcontactModalStatus] = useState({ show: false, data: {} });
 
-    useEffect(()=>{
-        setcontactModalStatus({ show: true, data: {} })
-    },[]);
+    // useEffect(()=>{
+    //     setcontactModalStatus({ show: true, data: {} })
+    // },[]);
   return (
     <div>
       Home
