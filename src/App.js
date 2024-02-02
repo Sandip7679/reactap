8fd8b2ef1e64f3f1594523f78f9e23bd33cca0f3
Sandip Bera <sandipbera.etc@gmail.com>
2024-02-02 18:51:41 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import { useState } from 'react';
 
 function App() {
-  const [mystate,setmystaate] = useState(false);
   return (
-    // <>
-    //   <Routes>
-    //     <Route path='/' element={<Home />} />
-    //   </Routes>
-    // </>
-    <div>
-      Hello World
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </>
+   
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import React from 'react';
+import React,{useState} from 'react';
 
 const Home = () => {
     // const [contactModalStatus, setcontactModalStatus] = useState({ show: false, data: {} });
