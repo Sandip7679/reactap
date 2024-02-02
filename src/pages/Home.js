813ef8e1bcb87ac96bb9542547c436ebcd1219ff
Sandip Bera <sandipbera.etc@gmail.com>
2024-02-02 19:42:39 +0530

    gdgh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,9 +19,9 @@ const Home = () => {
                 <button className='px-2 lg:px-5 py-2 border-black bg-white hover:bg-gray-800 hover:text-white border-[1px] border-r-0'>
                     Agent Property
                 </button>
-                <button className='px-2 lg:px-5 py-2 border-black bg-white hover:bg-gray-800 hover:text-white border-[1px]'>
+                {/* <button className='px-2 lg:px-5 py-2 border-black bg-white hover:bg-gray-800 hover:text-white border-[1px]'>
                     New Project
-                </button>
+                </button> */}
             </div>
             {/* <SearchIcon
                 imageClass='w-5 h-5 absolute left-2 top-[1px]  md:top-2'
