d23fc20d0edbcf55f848dcbb313fac7f18ef2100
Sandip Bera <sandipbera.etc@gmail.com>
2024-02-02 19:55:51 +0530

    savg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 // import Dropdown, { SearchIcon } from '../components/svgIcons';
 
 const Home = () => {
-    // const [contactModalStatus, setcontactModalStatus] = useState({ show: false, data: {} });
+    const [contactModalStatus, setcontactModalStatus] = useState({ show: false, data: {} });
 
-    // useEffect(()=>{
-    //     setcontactModalStatus({ show: true, data: {} })
-    // },[]);
+    useEffect(()=>{
+        setcontactModalStatus({ show: true, data: {} })
+    },[]);
     return (
         <div>
             <div className='sm:flex border-black '>
@@ -19,9 +19,9 @@ const Home = () => {
                 <button className='px-2 lg:px-5 py-2 border-black bg-white hover:bg-gray-800 hover:text-white border-[1px] border-r-0'>
                     Agent Property
                 </button>
-                {/* <button className='px-2 lg:px-5 py-2 border-black bg-white hover:bg-gray-800 hover:text-white border-[1px]'>
+                {contactModalStatus && <button className='px-2 lg:px-5 py-2 border-black bg-white hover:bg-gray-800 hover:text-white border-[1px]'>
                     New Project
-                </button> */}
+                </button>}
             </div>
             {/* <SearchIcon
                 imageClass='w-5 h-5 absolute left-2 top-[1px]  md:top-2'
